83d34cb91b950ce0b6c8c3f3969893e695b2c2c2
Gulnaz <egredimini@gmail.com>
2022-05-23 20:31:12 -0700

    feat: add portfolio section scripts

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -15,3 +15,41 @@ navLink.forEach(n => n.addEventListener("click", () => {
   document.body.classList.remove("lock");
 }));
 
+//Preload images
+function preloadImages() {
+  const seasons = ["winter", "spring", "summer", "autumn"];
+  seasons.forEach((season) => {
+    for (let i = 1; i <= 6; i++) {
+      const img = new Image();
+      img.src = `./img/${season}/${i}.jpg`;
+    }
+  });
+};
+
+// Change images
+const portfolioButtons = document.querySelector(".portfolio__buttons");
+const portfolioImages = document.querySelectorAll(".portfolio__image");
+
+function changePath(target) {
+  const season = target.dataset.season;
+  portfolioImages.forEach((img, index) => {
+    img.src = `./img/${season}/${index + 1}.jpg`;
+    img.alt = `${season} photography`;
+  });
+};
+
+function changeButton(targetButton) {
+  const activeButton = document.querySelector(".portfolio__button--active");
+  activeButton.classList.remove("portfolio__button--active");
+  targetButton.classList.add("portfolio__button--active");
+};
+
+function changeImage(e) {
+  if (e.target.classList.contains("portfolio__button") && !e.target.classList.contains("portfolio__button--active")) {
+    changePath(e.target);
+    changeButton(e.target);
+  }
+};
+
+portfolioButtons.addEventListener("click", changeImage);
+

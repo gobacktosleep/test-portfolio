c91a891fb1be01082ea6bedbc9f4ef0be9260891
Gulnaz <egredimini@gmail.com>
2022-05-23 21:29:13 -0700

    feat: add change theme script

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -57,6 +57,49 @@ navLink.forEach(n => n.addEventListener("click", () => {
   document.body.classList.remove("lock");
 }));
 
+//Change theme
+const storageKey = "theme";
+
+const onClick = () => {
+  theme.value = theme.value === "light" ? "dark" : "light";
+
+  setPreference();
+}
+
+const getColorPreference = () => {
+  if (localStorage.getItem(storageKey)) return localStorage.getItem(storageKey);
+  else return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+};
+
+const setPreference = () => {
+  localStorage.setItem(storageKey, theme.value);
+  reflectPreference();
+};
+
+const reflectPreference = () => {
+  document.firstElementChild.setAttribute("data-theme", theme.value);
+  document.querySelector(".theme-toggle")?.setAttribute("aria-label", theme.value);
+};
+
+const theme = {
+  value: getColorPreference()
+};
+
+reflectPreference();
+
+window.onload = () => {
+  reflectPreference();
+  document.querySelector(".theme-toggle").addEventListener("click", onClick);
+};
+
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", ({ matches: isDark }) => {
+    theme.value = isDark ? "dark" : "light";
+    setPreference();
+  });
+
+
 //Preload images
 function preloadImages() {
   const seasons = ["winter", "spring", "summer", "autumn"];
@@ -67,6 +110,8 @@ function preloadImages() {
     }
   });
 };
+
+preloadImages();
 
 // Change images
 const portfolioButtons = document.querySelector(".portfolio__buttons");
@@ -94,4 +139,3 @@ function changeImage(e) {
 };
 
 portfolioButtons.addEventListener("click", changeImage);
-

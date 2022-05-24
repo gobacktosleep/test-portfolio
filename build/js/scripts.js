e590c3bc0f6fc7966dee78599f4702b45d2f4eb0
Gulnaz <egredimini@gmail.com>
2022-05-23 19:44:55 -0700

    feat: add burger script

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,1 +1,17 @@
+// Burger script
+const menuToggle = document.querySelector(".burger");
+const nav = document.querySelector(".menu");
+const navLink = document.querySelectorAll(".menu__link");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("opened");
+  menuToggle.classList.toggle("opened");
+  document.body.classList.toggle("lock");
+});
+
+navLink.forEach(n => n.addEventListener("click", () => {
+  nav.classList.remove("opened");
+  menuToggle.classList.remove("opened");
+  document.body.classList.remove("lock");
+}));
 
